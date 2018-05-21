ede378ec7cd67773afae7eb73983aa93aff07590
JasonLong24 <jasonlongball@gmail.com>
2018-05-20 20:41:25 -0400

    abbs list makes a tree of the file structure

--- abbs.sh
+++ abbs.sh
@@ -2,6 +2,15 @@
 
 path=$(pwd)
 
+reset=$(tput sgr0)
+red=$(tput setaf 1) 
+green=$(tput setaf 2)
+yellow=$(tput setaf 3)
+blue=$(tput setaf 4)
+purple=$(tput setaf 5)
+cyan=$(tput setaf 6) 
+white=$(tput setaf 7)
+
 function newBlog() {
   read -p 'Blog name? ' bName
   echo $bName
@@ -24,6 +33,22 @@ function newEntry() {
     read -p 'Entry name? ' eName
     touch $path/$eName.md
     $EDITOR $path/$eName.md
+  else
+    echo -e 'This is not a blog directory.\nUse newBlog to initialize a blog.'
+  fi
+}
+
+function list() {
+  if [[ -f .blog ]] || [[ -f ../.blog ]]; then
+    echo -e 'Layout for current abbs projects \n' 
+    dirCount=$(echo */ | wc | awk '{print $2}' | tr -d ' ')
+    for ((n=1;n<$dirCount;n++))
+    do
+      dirname=$(echo */ | tr -d '/' | sed -e 's/build //g' | awk '{print $number}' number=$n)
+      filename=$(ls -p --ignore='*~' $dirname | tr -d '.md' | sed 's/^/  | /')
+      echo $blue$dirname$reset
+      echo -e "$filename"
+    done
   else
     echo -e 'This is not a blog directory.\nUse newBlog to initialize a blog.'
   fi
